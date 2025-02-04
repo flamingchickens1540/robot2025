b9234cec9f37bb9e58162f9a0da7b289543980c1
PichuIsBest <maddoxrg@icloud.com>
2025-02-04 15:45:06 -0800

    fix: fixed location of my constants

--- src/main/java/org/team1540/robot2025/Constants.java
+++ src/main/java/org/team1540/robot2025/Constants.java
@@ -28,23 +28,4 @@ public final class Constants {
         /** Replaying from a log file. */
         REPLAY
     }
-
-    public static class IntakeConstants {
-
-        // TODO: tune
-
-        public static final int TOP_FALCON_ID = 0;
-        public static final int BOTTOM_FALCON_ID = 1;
-        public static final int NEO_ID = 2;
-
-        public static final double GRABBER_KS = 0.25;
-        public static final double GRABBER_KV = 0.12;
-        public static final double GRABBER_KA = 0.01;
-        public static final double GRABBER_KP = 2.4;
-        public static final double GRABBER_KI = 0;
-        public static final double GRABBER_KD = 0.1;
-
-        public static final int GRABBER_CRUISE_VELOCITY = 80;
-        public static final int GRABBER_ACCELERATION = 160;
-    }
 }
---
+++ src/main/java/org/team1540/robot2025/subsystems/intake/IntakeConstants.java
@@ -0,0 +1,21 @@
+package org.team1540.robot2025.subsystems.intake;
+
+public class IntakeConstants {
+
+    // TODO: tune
+
+    public static final int SPIN_FALCON_ID = 0;
+    public static final int PIVOT_FALCON_ID = 1;
+    public static final int NEO_ID = 2;
+
+    public static final double GRABBER_KS = 0.25;
+    public static final double GRABBER_KV = 0.12;
+    public static final double GRABBER_KA = 0.01;
+    public static final double GRABBER_KP = 2.4;
+    public static final double GRABBER_KI = 0;
+    public static final double GRABBER_KD = 0.1;
+
+    public static final int GRABBER_CRUISE_VELOCITY = 80;
+    public static final int GRABBER_ACCELERATION = 160;
+
+}
