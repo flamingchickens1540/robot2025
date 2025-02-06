06ce13bcdbba6db4ac10cc19d90bbae0f49e9e89
PichuIsBest <maddoxrg@icloud.com>
2025-02-06 15:48:55 -0800

    fix: correctly cached status signals, set funnelEncoder at start

--- src/main/java/org/team1540/robot2025/subsystems/intake/IntakeIOReal.java
+++ src/main/java/org/team1540/robot2025/subsystems/intake/IntakeIOReal.java
@@ -1,6 +1,6 @@
 package org.team1540.robot2025.subsystems.intake;
 
-import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
@@ -16,19 +16,36 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.*;
 
 public class IntakeIOReal implements IntakeIO {
 
     // rotation of horizontal beams for the intake, clockwise to intake, counter-clockwise to spit out
     private final TalonFX spinFalcon = new TalonFX(IntakeConstants.SPIN_FALCON_ID);
 
+    private final StatusSignal<AngularVelocity> spinVelocity = spinFalcon.getVelocity();
+    private final StatusSignal<Angle> spinPosition = spinFalcon.getPosition();
+    private final StatusSignal<Voltage> spinAppliedVoltage = spinFalcon.getMotorVoltage();
+    private final StatusSignal<Current> spinSupplyCurrent = spinFalcon.getSupplyCurrent();
+    private final StatusSignal<Temperature> spinTemp = spinFalcon.getDeviceTemp();
+    private final StatusSignal<Current> spinStatorCurrent = spinFalcon.getStatorCurrent();
+
     // controls position of intake
     private final TalonFX pivotFalcon = new TalonFX(IntakeConstants.PIVOT_FALCON_ID);
+
+    private final StatusSignal<AngularVelocity> pivotVelocity = pivotFalcon.getVelocity();
+    private final StatusSignal<Angle> pivotPosition = pivotFalcon.getPosition();
+    private final StatusSignal<Voltage> pivotAppliedVoltage = pivotFalcon.getMotorVoltage();
+    private final StatusSignal<Current> pivotSupplyCurrent = pivotFalcon.getSupplyCurrent();
+    private final StatusSignal<Temperature> pivotTemp = pivotFalcon.getDeviceTemp();
+    private final StatusSignal<Current> pivotStatorCurrent = pivotFalcon.getStatorCurrent();
 
     private final MotionMagicVoltage pivotPositionRequest = new MotionMagicVoltage(0).withSlot(0);
 
     // clockwise to intake, counter-clockwise to spit out
     private final SparkMax funnelNeo = new SparkMax(IntakeConstants.NEO_ID, SparkLowLevel.MotorType.kBrushless);
+
+    RelativeEncoder funnelEncoder = funnelNeo.getEncoder();
 
     public IntakeIOReal() {
 
@@ -98,18 +115,30 @@ public class IntakeIOReal implements IntakeIO {
 
     @Override
     public void updateInputs(CoralIntakeInputs inputs) {
+        BaseStatusSignal.refreshAll(
+                spinVelocity,
+                spinPosition,
+                spinAppliedVoltage,
+                spinSupplyCurrent,
+                spinTemp,
+                spinStatorCurrent,
+                pivotVelocity,
+                pivotPosition,
+                pivotAppliedVoltage,
+                pivotSupplyCurrent,
+                pivotTemp,
+                pivotStatorCurrent);
 
-        inputs.spinMotorPosition = spinFalcon.getPosition().getValueAsDouble();
-        inputs.spinMotorVelocityRPS = spinFalcon.getVelocity().getValueAsDouble();
-        inputs.spinMotorAppliedVolts = spinFalcon.getMotorVoltage().getValueAsDouble();
-        inputs.spinCurrentAmps = spinFalcon.getStatorCurrent().getValueAsDouble();
+        inputs.spinMotorPosition = spinPosition.getValueAsDouble();
+        inputs.spinMotorVelocityRPS = spinVelocity.getValueAsDouble();
+        inputs.spinMotorAppliedVolts = spinAppliedVoltage.getValueAsDouble();
+        inputs.spinCurrentAmps = spinStatorCurrent.getValueAsDouble();
 
-        inputs.pivotMotorPosition = pivotFalcon.getPosition().getValueAsDouble();
-        inputs.pivotMotorVelocityRPS = pivotFalcon.getVelocity().getValueAsDouble();
-        inputs.pivotMotorAppliedVolts = pivotFalcon.getMotorVoltage().getValueAsDouble();
-        inputs.pivotCurrentAmps = pivotFalcon.getStatorCurrent().getValueAsDouble();
+        inputs.pivotMotorPosition = pivotPosition.getValueAsDouble();
+        inputs.pivotMotorVelocityRPS = pivotVelocity.getValueAsDouble();
+        inputs.pivotMotorAppliedVolts = pivotAppliedVoltage.getValueAsDouble();
+        inputs.pivotCurrentAmps = pivotStatorCurrent.getValueAsDouble();
 
-        RelativeEncoder funnelEncoder = funnelNeo.getEncoder();
         inputs.funnelMotorPosition = funnelEncoder.getPosition();
         inputs.funnelMotorVelocityRPS = funnelEncoder.getVelocity();
         inputs.funnelCurrentAmps = funnelNeo.getOutputCurrent();
