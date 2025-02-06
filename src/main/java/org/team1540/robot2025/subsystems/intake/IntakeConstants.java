99ce831156a9fe17c5696c87353555c125dc7887
PichuIsBest <maddoxrg@icloud.com>
2025-02-05 17:47:23 -0800

    feat: added more to pivot and spin config settings, fix: renamed constants

--- src/main/java/org/team1540/robot2025/subsystems/intake/IntakeConstants.java
+++ src/main/java/org/team1540/robot2025/subsystems/intake/IntakeConstants.java
@@ -8,13 +8,13 @@ public class IntakeConstants {
     public static final int PIVOT_FALCON_ID = 1;
     public static final int NEO_ID = 2;
 
-    public static final double INTAKE_KS = 0.25;
-    public static final double INTAKE_KV = 0.12;
-    public static final double INTAKE_KA = 0.01;
-    public static final double INTAKE_KP = 2.4;
-    public static final double INTAKE_KI = 0;
-    public static final double INTAKE_KD = 0.1;
+    public static final double PIVOT_INTAKE_KS = 0.25;
+    public static final double PIVOT_INTAKE_KV = 0.12;
+    public static final double PIVOT_INTAKE_KA = 0.01;
+    public static final double PIVOT_INTAKE_KP = 2.4;
+    public static final double PIVOT_INTAKE_KI = 0;
+    public static final double PIVOT_INTAKE_KD = 0.1;
 
-    public static final int INTAKE_CRUISE_VELOCITY = 80;
-    public static final int INTAKE_ACCELERATION = 160;
+    public static final int PIVOT_INTAKE_CRUISE_VELOCITY = 80;
+    public static final int PIVOT_INTAKE_ACCELERATION = 160;
 }
--- src/main/java/org/team1540/robot2025/subsystems/intake/IntakeIOReal.java
+++ src/main/java/org/team1540/robot2025/subsystems/intake/IntakeIOReal.java
@@ -2,7 +2,6 @@ package org.team1540.robot2025.subsystems.intake;
 
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -32,28 +31,34 @@ public class IntakeIOReal implements IntakeIO {
 
         spinTalonFXConfigs.CurrentLimits.withStatorCurrentLimitEnable(true);
         spinTalonFXConfigs.CurrentLimits.withStatorCurrentLimit(120);
+        spinTalonFXConfigs.CurrentLimits.withSupplyCurrentLimit(70);
+        spinTalonFXConfigs.CurrentLimits.withSupplyCurrentLowerLimit(40);
+        spinTalonFXConfigs.CurrentLimits.withSupplyCurrentLowerTime(0.5);
         spinTalonFXConfigs.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-        spinTalonFXConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        spinTalonFXConfigs.MotorOutput.NeutralMode = NeutralModeValue.Coast;
 
         pivotTalonFXConfigs.CurrentLimits.withStatorCurrentLimitEnable(true);
         pivotTalonFXConfigs.CurrentLimits.withStatorCurrentLimit(120);
+        pivotTalonFXConfigs.CurrentLimits.withSupplyCurrentLimit(70);
+        pivotTalonFXConfigs.CurrentLimits.withSupplyCurrentLowerLimit(40);
+        pivotTalonFXConfigs.CurrentLimits.withSupplyCurrentLowerTime(0.5);
         pivotTalonFXConfigs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
         pivotTalonFXConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
         Slot0Configs spinConfigs = spinTalonFXConfigs.Slot0;
         Slot0Configs pivotConfigs = pivotTalonFXConfigs.Slot0;
 
-        pivotConfigs.kS = IntakeConstants.INTAKE_KS;
-        pivotConfigs.kV = IntakeConstants.INTAKE_KV;
-        pivotConfigs.kA = IntakeConstants.INTAKE_KA;
-        pivotConfigs.kP = IntakeConstants.INTAKE_KP;
-        pivotConfigs.kI = IntakeConstants.INTAKE_KI;
-        pivotConfigs.kD = IntakeConstants.INTAKE_KD;
+        pivotConfigs.kS = IntakeConstants.PIVOT_INTAKE_KS;
+        pivotConfigs.kV = IntakeConstants.PIVOT_INTAKE_KV;
+        pivotConfigs.kA = IntakeConstants.PIVOT_INTAKE_KA;
+        pivotConfigs.kP = IntakeConstants.PIVOT_INTAKE_KP;
+        pivotConfigs.kI = IntakeConstants.PIVOT_INTAKE_KI;
+        pivotConfigs.kD = IntakeConstants.PIVOT_INTAKE_KD;
 
         MotionMagicConfigs motionMagicConfigs = pivotTalonFXConfigs.MotionMagic;
 
-        motionMagicConfigs.MotionMagicCruiseVelocity = IntakeConstants.INTAKE_CRUISE_VELOCITY;
-        motionMagicConfigs.MotionMagicAcceleration = IntakeConstants.INTAKE_ACCELERATION;
+        motionMagicConfigs.MotionMagicCruiseVelocity = IntakeConstants.PIVOT_INTAKE_CRUISE_VELOCITY;
+        motionMagicConfigs.MotionMagicAcceleration = IntakeConstants.PIVOT_INTAKE_ACCELERATION;
 
         spinFalcon.getConfigurator().apply(spinConfigs);
         pivotFalcon.getConfigurator().apply(pivotConfigs);
