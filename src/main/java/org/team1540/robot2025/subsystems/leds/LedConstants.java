9cf2131013aa468bc1a44bdba68c052004d17e7e
Zach Rutman <zarutman+git@gmail.com>
2025-02-15 12:00:41 -0800

    LED Subsystem (#10)

---
+++ src/main/java/org/team1540/robot2025/subsystems/leds/LedConstants.java
@@ -0,0 +1,7 @@
+package org.team1540.robot2025.subsystems.leds;
+
+public class LedConstants {
+    // TODO: Fix these
+    public static final int LEDS_PWM_PORT = 8;
+    public static final int LEDS_LENGTH = 30;
+}
