232843caa3df778ae45d7a4b1d652f7b670f1375
Zach Rutman <zarutman+git@gmail.com>
2025-02-15 15:06:39 -0800

    feat: better moving rainbow pattern

--- src/main/java/org/team1540/robot2025/subsystems/leds/LedConstants.java
+++ src/main/java/org/team1540/robot2025/subsystems/leds/LedConstants.java
@@ -2,6 +2,6 @@ package org.team1540.robot2025.subsystems.leds;
 
 public class LedConstants {
     // TODO: Fix these
-    public static final int LEDS_PWM_PORT = 8;
-    public static final int LEDS_LENGTH = 30;
+    public static final int LEDS_PWM_PORT = 0;
+    public static final int LEDS_LENGTH = 35;
 }
