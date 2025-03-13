6351cdabfb72de45cfe390d49430463ca11f2a73
Simon Weil <113215817+WeilSimon@users.noreply.github.com>
2025-03-13 10:45:39 -0700

    feat: arm setpoint plumbing

--- src/main/java/org/team1540/robot2025/subsystems/arm/Arm.java
+++ src/main/java/org/team1540/robot2025/subsystems/arm/Arm.java
@@ -119,8 +119,12 @@ public class Arm extends SubsystemBase {
         return setpoint;
     }
 
+    public Command commandToSepoint(Rotation2d position){
+        return Commands.run(() -> setPosition(position), this).until(this::isAtSetpoint);
+    }
+
     public Command commandToSetpoint(ArmState state) {
-        return Commands.run(() -> setPosition(state.position()), this).until(this::isAtSetpoint);
+        return commandToSepoint(state.position());
     }
 
     @AutoLogOutput(key = "Arm/TimeToSetpoint")
