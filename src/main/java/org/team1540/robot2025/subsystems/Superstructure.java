4c4c283e71602e17f3376f36e82d2124a402fac2
Alvin Zhang <41vin2h4n9@gmail.com>
2025-03-22 10:08:51 -0700

    Update at 'Sat Mar 22 10:08:51 PDT 2025'

--- src/main/java/org/team1540/robot2025/subsystems/Superstructure.java
+++ src/main/java/org/team1540/robot2025/subsystems/Superstructure.java
@@ -245,7 +245,7 @@ public class Superstructure {
                             case L1_FRONT -> intake.commandRunRollerFunnel(-0.3, -0.3)
                                     .withDeadline(Commands.waitUntil(() -> !intake.hasCoral())
                                             .andThen(Commands.waitSeconds(0.5)));
-                            case L2_FRONT, L3_FRONT -> grabber.commandRun(-0.45)
+                            case L2_FRONT, L3_FRONT -> grabber.commandRun(-0.4)
                                     .withDeadline(Commands.waitUntil(() -> !grabber.forwardSensorTripped())
                                             .andThen(Commands.waitSeconds(0.25)));
                             case L4_FRONT -> grabber.commandRun(0.1)
