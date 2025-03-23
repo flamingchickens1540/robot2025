c88e458e2d4761ed18d8a3f037b1017840c3791e
Alvin Zhang <41vin2h4n9@gmail.com>
2025-03-23 15:15:43 -0700

    Update at 'Sun Mar 23 15:15:43 PDT 2025'

--- src/main/java/org/team1540/robot2025/subsystems/Superstructure.java
+++ src/main/java/org/team1540/robot2025/subsystems/Superstructure.java
@@ -280,7 +280,7 @@ public class Superstructure {
                             case SCORE_BARGE_FRONT, SCORE_BARGE_BACK -> grabber.commandRun(-1.0)
                                     .withTimeout(0.5)
                                     .alongWith(Commands.runOnce(arm::holdPosition));
-                            default -> grabber.hasAlgae() ? grabber.commandRun(-0.5).withTimeout(0.25) : grabber.commandStartRun(0);
+                            default -> grabber.hasAlgae() ? grabber.commandRun(-0.5).withTimeout(0.5) : grabber.commandStartRun(0);
                         },
                         Set.of(elevator, arm, intake, grabber))
                 .andThen(stow().onlyIf(() -> stow));
