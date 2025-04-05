8cf50f9fb7aed041883a21c77a6b15378df3e9b0
Alvin Zhang <41vin2h4n9@gmail.com>
2025-04-05 14:09:24 -0700

    Update at 'Sat Apr 05 14:09:24 PDT 2025'

--- src/main/java/org/team1540/robot2025/commands/AutoScoreCommands.java
+++ src/main/java/org/team1540/robot2025/commands/AutoScoreCommands.java
@@ -59,7 +59,7 @@ public class AutoScoreCommands {
                                             .scoreCoral(height, () -> reverse)
                                             .asProxy(),
                                     Commands.waitSeconds(0.2).onlyIf(() -> !reverse && height != ReefHeight.L4),
-                                    Commands.waitSeconds(0.35).onlyIf(DriverStation::isAutonomous),
+                                    Commands.waitSeconds(0.25).onlyIf(DriverStation::isAutonomous),
                                     //                                    Commands.waitSeconds(0.2).onlyIf(() ->
                                     // !reverse && height == ReefHeight.L4),
                                     superstructure.score(false).asProxy());
